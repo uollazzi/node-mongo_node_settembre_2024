e5b2fd678cb284d60416ffeda3c50a363f0edbc4
Matteo Valenzi <dilloawallace@hotmail.com>
2024-09-11 21:09:37 +0200

    mongoose install

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,16 @@ import * as driver from "./driver";
 
 const mainDriver = async () => {
     // console.log(await driver.insertProdotto("Cipolle", 7));
-
     console.log(await driver.deleteProdotto("66e1d5a19f9e71e1a38c7761"));
     console.log(await driver.getProdotti(4));
+}
+
+const mainORM = async () => {
 
 }
 
-mainDriver();
+// mainDriver();
+mainORM();
 
 
 
